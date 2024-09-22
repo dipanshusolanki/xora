bd5099f69a61c670c76805583b4c16c0e4e23db4
Deepanshu Solanki <deepanshusolanki48@gmail.com>
2024-09-23 00:28:16 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+import Features from "./sections/Features.jsx";
+import Pricing from "./sections/Pricing.jsx";
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+    </main>
+  );
+};
+export default App;
