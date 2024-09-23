c66aafe945eaae6f8992295aaef80a74f726fc41
Deepanshu Solanki <deepanshusolanki48@gmail.com>
2024-09-24 02:05:20 +0530

    Completed XORA Static Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
+import Faq from "./sections/Faq.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
+import Download from "./sections/Download.jsx";
+import Footer from "./sections/Footer.jsx";
 
 const App = () => {
   return (
@@ -10,6 +14,10 @@ const App = () => {
       <Hero />
       <Features />
       <Pricing />
+      <Faq />
+      <Testimonials />
+      <Download />
+      <Footer />
     </main>
   );
 };
